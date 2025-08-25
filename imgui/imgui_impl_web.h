bb69af4b79fd6147d6edaa419395ad4dc9d34d53
Arcie Ren <r2277471064@163.com>
2025-08-25 19:39:10 +0800

    improve readability

--- imgui/imgui_impl_web.h
+++ imgui/imgui_impl_web.h
@@ -1,12 +1,10 @@
 #pragma once
 #include "imgui.h"
 
-// 极简 Emscripten/HTML5 平台层：只处理输入与帧大小
-// （渲染走官方 imgui_impl_opengl3）
 namespace ImGui_ImplWeb {
-  bool Init(void* native_window = nullptr); // 传 canvas 指针可忽略
-  void NewFrame(int fb_w, int fb_h, double dt);
-  void Shutdown();
+  bool Init (void* native_window = nullptr);
+  void NewFrame (int fbW, int fbH, double dt_ms);
+  void Shutdown ();
 }
 
 
